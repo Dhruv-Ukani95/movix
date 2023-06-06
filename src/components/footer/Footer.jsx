6a7e350f09655416fde0c72bf385e49230f159f7
Ukani_Dhruv <ukanidhruv008@gmail.com>
2023-06-06 18:06:23 +0530

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import {
+    FaFacebookF,
+    FaInstagram,
+    FaTwitter,
+    FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./style.scss";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <ContentWrapper>
+                <ul className="menuItems">
+                    <li className="menuItem">Terms Of Use</li>
+                    <li className="menuItem">Privacy-Policy</li>
+                    <li className="menuItem">About</li>
+                    <li className="menuItem">Blog</li>
+                    <li className="menuItem">FAQ</li>
+                </ul>
+                <div className="infoText">
+                   Movix website is Build for to inform The User About
+                   new movies, Reviews, Rating and also for see The Trailer 
+                   of The movies. So User can Decide Whather They Want to go
+                   Cenema For Watch that movie or not. This website Provides you
+                   The Letest information about the movies. User can Trust This
+                   website and Decide on their own without getting Influenced 
+                   By Other fake website..
+                </div>
+                <div className="socialIcons">
+                    <span className="icon">
+                        <FaFacebookF />
+                    </span>
+                    <span className="icon">
+                        <FaInstagram />
+                    </span>
+                    <span className="icon">
+                        <FaTwitter />
+                    </span>
+                    <span className="icon">
+                        <FaLinkedin />
+                    </span>
+                </div>
+            </ContentWrapper>
+        </footer>
+    );
+};
+
+export default Footer;
